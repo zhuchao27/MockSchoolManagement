26187c556f2675ebb9c5f6b0bb0b846489f51d75
zhu chao <13777887362@163.com>
2021-01-26 14:06:28 +0800

    新增文件

---
+++ MockSchoolManagement/Controllers/ErrorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "抱歉，访问页面不存在。";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    logger.LogWarning($"发生了一个404错误。路径= " + $"{statusCodeResult.OriginalPath} 以及查询字符串=" + $"{statusCodeResult.OriginalQueryString}");
+                    break;
+            }
+            return View("NotFound");
+        }
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            logger.LogError($"路径 {exceptionHandlerPathFeature.Path} " + $"产生了一个错误{exceptionHandlerPathFeature.Error}");
+
+            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+
+            return View("Error");
+        }
+
+    }
+}
---
+++ MockSchoolManagement/Extension/EnumExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.Extension
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this System.Enum en)
+        {
+            Type type = en.GetType();
+            MemberInfo[] memberInfos = type.GetMember(en.ToString());
+            if (memberInfos != null && memberInfos.Length > 0)
+            {
+                object[] attrs = memberInfos[0].GetCustomAttributes(typeof(DisplayAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DisplayAttribute)attrs[0]).Name;
+                }
+            }
+            return en.ToString();
+        }
+    }
+}
